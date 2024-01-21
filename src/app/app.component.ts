77808fb666c2b77fa3b7c751ba72d0db1d6bca65
lucyogutu <amondilucy192@gmail.com>
2024-01-21 17:10:13 +0300

    feat: top header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, TopHeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'ekart';
+  title = 'Ekart';
 }
