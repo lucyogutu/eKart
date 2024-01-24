508b99657385f1f71c8dc221a0ddf79a32a86d05
lucyogutu <amondilucy192@gmail.com>
2024-01-24 15:15:21 +0300

    feat: use of ng-content element

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -3,6 +3,7 @@ import { SearchComponent } from '../container/search/search.component';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { FeaturedBrandsComponent } from './featured-brands/featured-brands.component';
 
 @Component({
   selector: 'app-container',
@@ -12,6 +13,7 @@ import { ProductDetailComponent } from './product-detail/product-detail.componen
     CommonModule,
     ProductListComponent,
     ProductDetailComponent,
+    FeaturedBrandsComponent,
   ],
   templateUrl: './container.component.html',
   styleUrl: './container.component.css',
